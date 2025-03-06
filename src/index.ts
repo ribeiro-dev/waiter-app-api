5118b8631f71be856ae435a6604a7c2a3afd6a18
ribeiro-dev <romilson.2r@gmail.com>
2025-03-06 16:24:23 -0300

    finalizando dia 5 - integrando ui com backend

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
 import path from 'node:path';
+import http from 'node:http';
 
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
 import { router } from './router';
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose.connect('mongodb://localhost:27017')
    .then(() => {
-      const app = express();
       const PORT = 3001;
 
+      app.use((req, res, next) => {
+         res.setHeader('Access-Control-Allow-Origin', '*');
+         res.setHeader('Access-Control-Allow-Methods', '*');
+         res.setHeader('Access-Control-Allow-Headers', '*');
+
+         next();
+      });
       app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))) // set the static files folder
       app.use(express.json()); // Middleware to manipulate json in body requests
       app.use(router); // Defining the routes from the router
 
-      app.listen(PORT, () => {
+      server.listen(PORT, () => {
          console.log(`🚀 Server is running on http://localhost:${PORT}`);
       });
 
